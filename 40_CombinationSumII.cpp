68b21f351419590a7be37717eb8cdcbce54b6056
jeremy <651079789@qq.com>
2018-02-07 13:00:47 +0800

    .

---
+++ 40_CombinationSumII.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<vector<int>> res;
+		vector<int> tmp;
+		sort(candidates.begin(), candidates.end());
+		dfs(res,tmp,candidates,target,0);
+		return res;
+    }
+	int dfs(vector<vector<int>>& res,vector<int>& tmp,vector<int>& candidates,int target,int start)
+	{
+		if(target<0)
+		{
+			return 0;
+		}
+		if(target==0)
+		{
+			res.push_back(tmp);
+			return 0;
+		}
+		for(int i=start; i<candidates.size(); i++)
+		{
+                if(i!=start && candidates[i]==candidates[i-1])
+                continue;
+			//key作为树的枝减条件，当树搜索到正确的结果后，或者target为负后，就可以将右子树枝减掉
+			int key;
+		        tmp.push_back(candidates[i]);
+			key = dfs(res,tmp,candidates,target-candidates[i],i+1);
+           		tmp.pop_back();
+			if (key == 0)
+			{
+				break;
+			}
+		}
+		return 1;
+	}
+};
