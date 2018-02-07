446d63ff896a54590cc6f5a1c183b2f6ff5cb370
jeremy <651079789@qq.com>
2018-02-07 10:34:35 +0800

    .

--- 39_CombinationSum.cpp
+++ 39_CombinationSum.cpp
@@ -22,9 +22,9 @@ public:
 		{
 			//key作为树的枝减条件，当树搜索到正确的结果后，或者target为负后，就可以将右子树枝减掉
 			int key;
-            tmp.push_back(candidates[i]);
+		        tmp.push_back(candidates[i]);
 			key = dfs(res,tmp,candidates,target-candidates[i],i);
-            tmp.pop_back();
+           		 tmp.pop_back();
 			if (key == 0)
 			{
 				break;
