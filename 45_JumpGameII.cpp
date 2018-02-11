84620456d2e83b256f3b934448e0acef62f313fa
jeremy <651079789@qq.com>
2018-02-11 13:37:44 +0800

    .

--- 45_JumpGameII.cpp
+++ 45_JumpGameII.cpp
@@ -5,6 +5,8 @@ public:
         if(size==1)
             return 0;
 	int res = 0,current = 0;
+	//res存放已走的步数，current是当前所在的位置，max是当前位置后2步能到达的最大位置，tmp是存放下一次跳的位置
+	//每次跳像后思考2步，以后2步的最远距离的贪心解来解题
 	while(1)
 	{
 		int max = current,tmp = current;
