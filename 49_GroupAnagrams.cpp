4aa4e13a508aff284363cb0737c2c4413dc75939
jeremy <651079789@qq.com>
2018-02-14 18:44:30 +0800

    .

---
+++ 49_GroupAnagrams.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> res;
+		unordered_map<string,vector<string>> hash;
+		for(auto str:strs)
+		{
+			string tmp = str;
+			sort(tmp.begin(),tmp.end());
+			hash[tmp].push_back(str);
+		}
+		for(auto a:hash)
+		{
+			res.push_back(a.second);
+		}
+		return res;
+    }
+};
