1977895343f26b7e09a21f5b68937087f6639f04
jeremy <651079789@qq.com>
2018-09-22 14:04:49 +0800

    .

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -2,15 +2,13 @@ class Solution {
 public:
     int GetNumberOfK(vector<int> data ,int k) {
         int lower = getLower(data,k);
-        int upper = getUpper(data,k);
-         
+        int upper = getUpper(data,k);         
         return upper - lower + 1;
     }
      
     int getLower(vector<int> data,int k){
         int start = 0,end = data.size()-1;
         int mid = (start + end)/2;
-         
         while(start <= end){
             if(data[mid] < k){
                 start = mid + 1;
@@ -23,9 +21,8 @@ public:
     }
 
     int getUpper(vector<int> data,int k){
-         int start = 0,end = data.size()-1;
-        int mid = (start + end)/2;
-         
+        int start = 0,end = data.size()-1;
+        int mid = (start + end)/2; 
         while(start <= end){
             if(data[mid] <= k){
                 start = mid + 1;
